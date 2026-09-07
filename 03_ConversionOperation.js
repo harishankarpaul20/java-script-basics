b85113529d99ed0b3287a4bebc31a1cda8059eb5
harishankarpaul20 <harishankarpaul20@gmail.com>
2026-09-07 15:24:58 +0000

    conversion of datatypes

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInumber = Number(score)
+console.log(typeof(valueInumber))
+
+//true=>1 or false=>0;
+//let isLoggedin = 1;
+
+//let booleanIsloggedin = Boolean(isloggedin)
+//console.log(booleanIsloggedin)
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof(someNumber))
+
