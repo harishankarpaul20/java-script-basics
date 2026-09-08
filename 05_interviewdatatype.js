96d0aa08fb65aab1b2c2dbcd99ba943495662117
harishankarpaul20 <harishankarpaul20@gmail.com>
2026-09-08 15:02:45 +0000

    dataypes summary

--- 05_interviewdatatype.js
+++ 05_interviewdatatype.js
@@ -1,1 +1,33 @@
-//privitive data
+//privitive 
+//there are 7 types of data basacially : string , bollean, null, undefined , shymbol
+//big init
+
+const score = 100
+const scorevalue = 100.3;
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bignumber = 2948949462959556265n
+
+//reference(non - primitive )
+
+//array, objects, functions
+const heros = ["Ara","chapra","dharbhanga","siwan"]
+
+let myobj = {
+    name:"hitesh",
+    age:22,
+}
+
+
+
+const myfunction =  function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
